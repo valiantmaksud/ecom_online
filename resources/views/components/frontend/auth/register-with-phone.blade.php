108057659ad04be925101522e3ce6d8586c97c8d
Maksud <maksud.masum@softrobotics.com.bd>
2024-04-25 00:14:04 +0600

    update

--- resources/views/components/frontend/auth/register-with-phone.blade.php
+++ resources/views/components/frontend/auth/register-with-phone.blade.php
@@ -2,22 +2,18 @@
     <p>Please Enter Your Phone Number</p>
     <div class="input_group mb-3">
         <div class="input_img">
-            <img src="{{ asset('assets/frontend') }}/image/section_img/country.png" alt="image">
             +88
         </div>
         <input type="text" name="phone_number" value="{{ old('phone_number') }}">
-        <a href="javascrip:void(0)" class="input-group-text send-otp" id="send-btn">
+        {{-- <a href="javascrip:void(0)" class="input-group-text send-otp" id="send-btn">
             Send OTP
-          </a>
+          </a> --}}
     </div>
     
     <div class="input_group mb-3" id="verify-area" style="display: none">
         <div class="input_img">
             <span class="input-group-text" style="height: 100%;border:none"><i class="fas fa-check-circle fa-2x"></i></span>
         </div>
-        <input type="text" class="rounded-0" name="verification_code"
-            value="{{ old('verification_code') }}" id="verification_code" placeholder="Verification Code" autocomplete="off"
-            required>
     </div>
 
     <div class="input_group" id="reg-pass">
--- resources/views/frontend/auth/register.blade.php
+++ resources/views/frontend/auth/register.blade.php
@@ -14,9 +14,7 @@
                         <div class="login_btn">
                             <button type="submit" class="login_btn btn_log" id="register">SIGN UP</button>
                             <p>Or</p>
-                            <a class="btn_log face_log" href="#"> <i class="fab fa-facebook-f"></i> LOGIN WITH FACEBOOK</a>
-                            <a class="btn_log g_log" href="#"> <i class="fab fa-google"></i> LOGIN WITH GOOGLE</a>
-                            <a class="btn_log mail_log register-with-phone-or-email" href="javascript:void(0)"> <i class="fas fa-envelope"></i> LOGIN WITH E-MAIL</a>
+                            <a class="btn_log mail_log" href="/login">LOGIN</a>
                         </div>
                     </form>
                 </div>
